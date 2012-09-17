837dbe4bbddf61c459b309e5a7409c89cb544513
ih <irvin.hwang@gmail.co>
2012-09-17 08:40:28 -0700

    interpreter should be able to handle functions via substitution

--- assignment2/cfwae.rkt
+++ assignment2/cfwae.rkt
@@ -1,1 +1,31 @@
-#lang racket
+#lang plai
+
+(define-type ArithC
+  [numC (n : number)]
+  [plusC (l : ArithC) (r : ArithC)]
+  [multC (l : ArithC) (r : ArithC)]
+  [divCC (l : ArithC) (r : ArithC)])
+
+(define-type ArithS
+  [numS (n : number)]
+  [plusS (l : ArithS) (r : ArithS)]
+  [multS (l : ArithS) (r : ArithS)]
+  [bminusS (l : ArithS) (r : ArithS)])
+
+(define (parser sexp) : ArithS
+  (cond
+    [(s-exp-number? sexp) (numS (s-exp->number sexp))]
+    [(s-exp-list? sexp)
+     (let ([sexp-list (s-exp->list sexp)])
+       (case (s-exp->symbol (first sexp-list))
+         [(+) (plusS (parser (second sexp-list)) (parser (third sexp-list)))]
+         [(*) (multS (parser (second sexp-list)) (parser (third sexp-list)))]
+         [(-) (bminusS (parser (second sexp-list)) (parser (third sexp-list)))]))]))
+
+(define (interpret arithC-exp) : number
+  (type-case ArithC arithC-exp
+    [numC (num) num]
+    [plusC (l r) (+ (interpret l) (interpret r))]
+    [multC (l r) (* (interpret l) (interpret r))]))
+
+(test (interpret (parser '(+ 2 3))) 5)
--- book/cs173book.rkt
+++ book/cs173book.rkt
@@ -41,28 +41,47 @@
          [(-) (bminusS (parser (second sexp-list)) (parser (third sexp-list)))]
          [(if) (ifS (parser (second sexp-list)) (parser (third sexp-list)) (parser (fourth sexp-list)))]))]))
 
-;(define (interpret ExprC-exp) : number
-;  (type-case ExprC ExprC-exp
-;    [numC (num) num]
-;    [plusC (l r) (+ (interpret l) (interpret r))]
-;    [multC (l r) (* (interpret l) (interpret r))]
-;    [ifC (c l r) (if (= (interpret c) 0) (interpret r) (interpret l))]
-;    [idC (s) 3]
-;    [appC (fun arg) 5]
-;    [else (error 'interpret "Expression not interpretable")]))
+(define (interpret ExprC-exp fun-defs) : number
+  (type-case ExprC ExprC-exp
+    [numC (num) num]
+    [plusC (l r) (+ (interpret l fun-defs) (interpret r fun-defs))]
+    [multC (l r) (* (interpret l fun-defs) (interpret r fun-defs))]
+    [ifC (c l r) (if (= (interpret c fun-defs) 0) 
+                     (interpret r fun-defs) 
+                     (interpret l fun-defs))]
+    [idC (s) (error 'interpret "encountered a free variable!")]
+    [appC (fun arg) (let ([fun-def (get-def fun fun-defs)])
+                      (interpret (subst (fdC-arg fun-def) arg (fdC-body fun-def))
+                                 fun-defs))]))
 
-;(define (desugar ExprS-exp) : ExprC
-;  (type-case ExprS ExprS-exp
-;    [numS (num) (numC num)]
-;    [plusS (l r) (plusC (desugar l) (desugar r))]
-;    [multS (l r) (multC (desugar l) (desugar r))]
-;    [bminusS (l r) (plusC (desugar l) (multC (numC -1) (desugar r)))]
-;    [ifS (c l r) (ifC (desugar c) (desugar l) (desugar r))]))
+(define (get-def name fun-defs) : FunDefC
+  (cond 
+    [(empty? fun-defs) (error 'get-def "function not defined")]
+    [(eq? name (fdC-name (first fun-defs))) (first fun-defs)]
+    [else (get-def name (rest fun-defs))]))
 
+(define (desugar ExprS-exp) : ExprC
+  (type-case ExprS ExprS-exp
+    [numS (num) (numC num)]
+    [plusS (l r) (plusC (desugar l) (desugar r))]
+    [multS (l r) (multC (desugar l) (desugar r))]
+    [bminusS (l r) (plusC (desugar l) (multC (numC -1) (desugar r)))]
+    [ifS (c l r) (ifC (desugar c) (desugar l) (desugar r))]
+    [else (error 'desugar "can't desugar this case")]))
 
+(define (subst [variable : symbol] [argument : ExprC] [body : ExprC]) : ExprC
+  (type-case ExprC body
+    [numC (n) body]
+    [plusC (l r) (plusC (subst variable argument l) 
+                        (subst variable argument r))]
+    [multC (l r) (multC (subst variable argument l)
+                        (subst variable argument r))]
+    [ifC (c l r) (ifC (subst variable argument c)
+                      (subst variable argument l)
+                      (subst variable argument r))]
+    [idC (s) (if (eq? variable s) argument body)]
+    [appC (fun arg) (appC fun (subst variable argument arg))]))
 ;(test (interpret (parser '(+ 2 3))) 5)
-(type-case ExprC (numC 5)
-  [numC (num) num]
-  [else 3])
+
 
 
