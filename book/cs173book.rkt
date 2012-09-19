2ea0e70397ff863c44985a0f458ed57fbb94138f
ih <irvin.hwang@gmail.co>
2012-09-19 08:49:26 -0700

    changed interpreter to handle function definitions as values

--- book/cs173book.rkt
+++ book/cs173book.rkt
@@ -15,7 +15,8 @@
   [multC (l : ExprC) (r : ExprC)]
   [ifC (condition : ExprC) (branch1 : ExprC) (branch2 : ExprC)]
   [idC (s : symbol)]
-  [appC (fun : symbol) (argument : ExprC)])
+  [appC (fun : ExprC) (argument : ExprC)]
+  [fdC (name : symbol) (arg : symbol) (body : ExprC)])
 
 (define-type ExprS
   [numS (n : number)]
@@ -26,13 +27,17 @@
   [idS (variable : symbol)]
   [appS (fun : symbol) (arg : ExprS)])
 
-(define-type FunDefC
-  [fdC (name : symbol) (arg : symbol) (body : ExprC)])
+;(define-type FunDefC
+;  [fdC (name : symbol) (arg : symbol) (body : ExprC)])
 
 (define-type Binding
-  [bind (variable : symbol) (value : number)])
+  [bind (variable : symbol) (value : Value)])
 
 (define-type-alias Env (listof Binding))
+
+(define-type Value
+  [numV (n : number)]
+  [funV (name : symbol) (arg : symbol) (body : ExprC)])
 
 (define mt-env empty)
 (define extend-env cons)
@@ -49,30 +54,44 @@
          [(-) (bminusS (parser (second sexp-list)) (parser (third sexp-list)))]
          [(if) (ifS (parser (second sexp-list)) (parser (third sexp-list)) (parser (fourth sexp-list)))]))]))
 
-(define (interpret ExprC-exp environment fun-defs) : number
-  (type-case ExprC ExprC-exp
-    [numC (num) num]
-    [plusC (l r) (+ (interpret l environment fun-defs) 
-                    (interpret r environment fun-defs))]
-    [multC (l r) (* (interpret l environment fun-defs) 
-                    (interpret r environment fun-defs))]
-    [ifC (c l r) (if (= (interpret c environment fun-defs) 0) 
-                     (interpret r environment fun-defs) 
-                     (interpret l environment fun-defs))]
+(define (interpret [expr : ExprC] [environment : Env]) : Value
+  (type-case ExprC expr
+    [numC (num) (numV num)]
+    [plusC (l r) (num+ (interpret l environment) 
+                    (interpret r environment ))]
+    [multC (l r) (num* (interpret l environment) 
+                    (interpret r environment))]
+    [ifC (c l r) (if (num= (interpret c environment) (numV 0)) 
+                     (interpret r environment) 
+                     (interpret l environment))]
     [idC (s) (lookup s environment)]
-    [appC (fun arg) (local ([define fun-def (get-def fun fun-defs)])
+    [appC (fun arg) (local ([define fun-def (interpret fun environment)]) 
                       (interpret 
-                       (fdC-body fun-def)
-                       (extend-env (bind (fdC-arg fun-def) 
-                                         (interpret arg environment fun-defs)) 
-                                         mt-env)
-                       fun-defs))]))
+                         (funV-body fun-def)
+                         (extend-env (bind (funV-arg fun-def) 
+                                           (interpret arg environment)) 
+                                     mt-env)))]
+    [fdC (name arg body) (funV name arg body)]))
 
-(define (get-def name fun-defs) : FunDefC
+(define (num+ [l : Value] [r : Value]) : Value
   (cond 
-    [(empty? fun-defs) (error 'get-def "function not defined")]
-    [(eq? name (fdC-name (first fun-defs))) (first fun-defs)]
-    [else (get-def name (rest fun-defs))]))
+    [(and (numV? l) (numV? r)) (numV (+ (numV-n l) (numV-n r)))]
+    [else (error 'num+ "trying to add non-numV values")]))
+        
+(define (num* [l : Value] [r : Value]) : Value
+  (cond 
+    [(and (numV? l) (numV? r)) (numV (* (numV-n l) (numV-n r)))]
+    [else (error 'num* "trying to multiply non-numV values")]))
+
+(define (num= a b)
+  (cond
+    [(and (numV? a) (numV? b)) (= (numV-n a) (numV-n b))]
+    [else (error 'num= "trying to check equality between two non-values")]))
+;(define (get-def name fun-defs) : FunDefC
+;  (cond 
+;    [(empty? fun-defs) (error 'get-def "function not defined")]
+;    [(eq? name (fdC-name (first fun-defs))) (first fun-defs)]
+;    [else (get-def name (rest fun-defs))]))
 
 (define (desugar ExprS-exp) : ExprC
   (type-case ExprS ExprS-exp
@@ -83,38 +102,49 @@
     [ifS (c l r) (ifC (desugar c) (desugar l) (desugar r))]
     [else (error 'desugar "can't desugar this case")]))
 
-(define (subst [variable : symbol] [argument : ExprC] [body : ExprC]) : ExprC
-  (type-case ExprC body
-    [numC (n) body]
-    [plusC (l r) (plusC (subst variable argument l) 
-                        (subst variable argument r))]
-    [multC (l r) (multC (subst variable argument l)
-                        (subst variable argument r))]
-    [ifC (c l r) (ifC (subst variable argument c)
-                      (subst variable argument l)
-                      (subst variable argument r))]
-    [idC (s) (if (eq? variable s) argument body)]
-    [appC (fun arg) (appC fun (subst variable argument arg))]))
+;(define (subst [variable : symbol] [argument : ExprC] [body : ExprC]) : ExprC
+;  (type-case ExprC body
+;    [numC (n) body]
+;    [plusC (l r) (plusC (subst variable argument l) 
+;                        (subst variable argument r))]
+;    [multC (l r) (multC (subst variable argument l)
+;                        (subst variable argument r))]
+;    [ifC (c l r) (ifC (subst variable argument c)
+;                      (subst variable argument l)
+;                      (subst variable argument r))]
+;    [idC (s) (if (eq? variable s) argument body)]
+;    [appC (fun arg) (appC fun (subst variable argument arg))]))
 
-(define (lookup name env) : number
+(define (lookup name env) : Value
   (cond [(empty? env) (error 'lookup "variable not found")]
         [(eq? name (bind-variable (first env))) (bind-value (first env))]
         [else (lookup name (rest env))]))
 
-(test (interpret (plusC (numC 10) (appC 'const5 (numC 10)))
-                 mt-env
-              (list (fdC 'const5 '_ (numC 5)))) 15)
 
-(test (interpret (plusC (numC 10) (appC 'double (plusC (numC 1) (numC 2))))
-              mt-env
-              (list (fdC 'double 'x (plusC (idC 'x) (idC 'x)))))
-      16)
+(test (interpret (plusC (numC 10) (appC (fdC 'const5 '_ (numC 5)) (numC 10)))
+              mt-env)
+      (numV 15))
+ 
+(test/exn (interpret (appC (fdC 'f1 'x (appC (fdC 'f2 'y (plusC (idC 'x) (idC 'y)))
+                                          (numC 4)))
+                        (numC 3))
+                  mt-env)
+          "lookup: variable not found")
 
-(test (interpret (plusC (numC 10) (appC 'quadruple (plusC (numC 1) (numC 2))))
-              mt-env
-              (list (fdC 'quadruple 'x (appC 'double (appC 'double (idC 'x))))
-                    (fdC 'double 'x (plusC (idC 'x) (idC 'x)))))
-      22)
+;(test (interpret (plusC (numC 10) (appC 'const5 (numC 10)))
+;                 mt-env
+;              (list (fdC 'const5 '_ (numC 5)))) 15)
+;
+;(test (interpret (plusC (numC 10) (appC 'double (plusC (numC 1) (numC 2))))
+;              mt-env
+;              (list (fdC 'double 'x (plusC (idC 'x) (idC 'x)))))
+;      16)
+;
+;(test (interpret (plusC (numC 10) (appC 'quadruple (plusC (numC 1) (numC 2))))
+;              mt-env
+;              (list (fdC 'quadruple 'x (appC 'double (appC 'double (idC 'x))))
+;                    (fdC 'double 'x (plusC (idC 'x) (idC 'x)))))
+;      22)
 ;(test (interpret (parser '(+ 2 3))) 5)
 
 
